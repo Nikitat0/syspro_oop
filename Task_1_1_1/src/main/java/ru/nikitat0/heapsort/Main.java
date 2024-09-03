419ffde875499d7cb25afba90cbab7061c4a31f5
Nikitat0 <nikitatzero@yandex.ru>
2024-09-03 22:24:53 +0700

    Init Gradle

---
+++ Task_1_1_1/src/main/java/ru/nikitat0/heapsort/Main.java
@@ -0,0 +1,11 @@
+package ru.nikitat0.heapsort;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
---
+++ Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
@@ -0,0 +1,10 @@
+package ru.nikitat0.heapsort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+  @Test
+  void dummyTest() {}
+}
