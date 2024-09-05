b83d61a225af728e05b173151feb0e081cf8615c
Nikitat0 <nikitatzero@yandex.ru>
2024-09-05 13:39:41 +0700

    Apply Google Code Style

--- Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
@@ -2,33 +2,35 @@ package ru.nikitat0.heapsort;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.Random;
+import org.junit.jupiter.api.Test;
 
 class MainTest {
-    static int[] sampleArray() {
-        Random rd = new Random();
-        int[] sample = new int[100];
-        for (int i = 0; i < 100; i++) sample[i] = rd.nextInt();
-        return sample;
+  static int[] sampleArray() {
+    Random rd = new Random();
+    int[] sample = new int[100];
+    for (int i = 0; i < 100; i++) {
+      sample[i] = rd.nextInt();
     }
+    return sample;
+  }
 
-    @Test
-    void testMakeHeap() {
-        int[] sample = sampleArray();
-        Main.makeHeap(sample);
-        for (int i = 1; i < sample.length; i++)
-            assertTrue(sample[i] <= sample[(i - 1) / 2], "Heap invariant is violated");
+  @Test
+  void testMakeHeap() {
+    int[] sample = sampleArray();
+    Main.makeHeap(sample);
+    for (int i = 1; i < sample.length; i++) {
+      assertTrue(sample[i] <= sample[(i - 1) / 2], "Heap invariant is violated");
     }
+  }
 
-    @Test
-    void testHeapsort() {
-        int[] expected = sampleArray();
-        int[] actual = expected.clone();
-        Arrays.sort(expected);
-        Main.heapsort(actual);
-        assertArrayEquals(expected, actual);
-    }
+  @Test
+  void testHeapsort() {
+    int[] expected = sampleArray();
+    int[] actual = expected.clone();
+    Arrays.sort(expected);
+    Main.heapsort(actual);
+    assertArrayEquals(expected, actual);
+  }
 }
