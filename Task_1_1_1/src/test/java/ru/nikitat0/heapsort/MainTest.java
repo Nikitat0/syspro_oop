02d725bb44680e76331645768812df525f11d0ef
Nikitat0 <nikitatzero@yandex.ru>
2024-09-05 10:47:54 +0700

    Solve

--- Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
@@ -4,7 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 class MainTest {
-  @Test
-  void dummyTest() {}
+    static int[] sampleArray() {
+        Random rd = new Random();
+        int[] sample = new int[100];
+        for (int i = 0; i < 100; i++) sample[i] = rd.nextInt();
+        return sample;
+    }
+
+    @Test
+    void testMakeHeap() {
+        int[] sample = sampleArray();
+        Main.makeHeap(sample);
+        for (int i = 1; i < sample.length; i++)
+            assertTrue(sample[i] <= sample[(i - 1) / 2], "Heap invariant is violated");
+    }
+
+    @Test
+    void testHeapsort() {
+        int[] expected = sampleArray();
+        int[] actual = expected.clone();
+        Arrays.sort(expected);
+        Main.heapsort(actual);
+        assertArrayEquals(expected, actual);
+    }
 }
