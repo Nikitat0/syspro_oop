01bbae571a8d235dbf30f6ad11ec27d0806af596
Nikitat0 <nikitatzero@yandex.ru>
2024-09-06 08:16:36 +0700

    Switch to 4-space identation

--- Task_1_1_1/src/main/java/ru/nikitat0/heapsort/Main.java
+++ Task_1_1_1/src/main/java/ru/nikitat0/heapsort/Main.java
@@ -4,63 +4,63 @@ import java.util.Arrays;
 
 /** Contains all program function */
 public class Main {
-  /**
-   * Entry point
-   *
-   * @param args Program arguments
-   */
-  public static void main(String[] args) {
-    int[] array = new int[] {5, 4, 3, 2, 1};
-    heapsort(array);
-    System.out.println(Arrays.toString(array));
-  }
-
-  /**
-   * Constructs a binary heap from an array in-place.
-   *
-   * @param array Array with elements to make the heap
-   */
-  public static void heapsort(int[] array) {
-    makeHeap(array);
-    for (int i = array.length - 1; i > 0; i--) {
-      int j = 0;
-      int e = array[i];
-      array[i] = array[0];
-      array[j] = e;
-      while (true) {
-        int k = j;
-        int l = j * 2 + 1;
-        int r = j * 2 + 2;
-        if (l < i && array[l] > array[k]) {
-          k = l;
-        }
-        if (r < i && array[r] > array[k]) {
-          k = r;
-        }
-        if (j == k) {
-          break;
-        }
-        array[j] = array[k];
-        array[k] = e;
-        j = k;
-      }
+    /**
+     * Entry point
+     *
+     * @param args Program arguments
+     */
+    public static void main(String[] args) {
+        int[] array = new int[] { 5, 4, 3, 2, 1 };
+        heapsort(array);
+        System.out.println(Arrays.toString(array));
     }
-  }
 
-  /**
-   * Sorts an array.
-   *
-   * @param array Array to sort
-   */
-  public static void makeHeap(int[] array) {
-    for (int i = 1; i < array.length; i++) {
-      int j = i;
-      int e = array[j];
-      while (j != 0 && array[(j - 1) / 2] < e) {
-        array[j] = array[(j - 1) / 2];
-        array[(j - 1) / 2] = e;
-        j = (j - 1) / 2;
-      }
+    /**
+     * Constructs a binary heap from an array in-place.
+     *
+     * @param array Array with elements to make the heap
+     */
+    public static void heapsort(int[] array) {
+        makeHeap(array);
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = 0;
+            int e = array[i];
+            array[i] = array[0];
+            array[j] = e;
+            while (true) {
+                int k = j;
+                int l = j * 2 + 1;
+                int r = j * 2 + 2;
+                if (l < i && array[l] > array[k]) {
+                    k = l;
+                }
+                if (r < i && array[r] > array[k]) {
+                    k = r;
+                }
+                if (j == k) {
+                    break;
+                }
+                array[j] = array[k];
+                array[k] = e;
+                j = k;
+            }
+        }
     }
-  }
+
+    /**
+     * Sorts an array.
+     *
+     * @param array Array to sort
+     */
+    public static void makeHeap(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int j = i;
+            int e = array[j];
+            while (j != 0 && array[(j - 1) / 2] < e) {
+                array[j] = array[(j - 1) / 2];
+                array[(j - 1) / 2] = e;
+                j = (j - 1) / 2;
+            }
+        }
+    }
 }
--- Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nikitat0/heapsort/MainTest.java
@@ -6,30 +6,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
-  static int[] sampleArray() {
-    Random rd = new Random();
-    int[] sample = new int[100];
-    for (int i = 0; i < 100; i++) {
-      sample[i] = rd.nextInt();
+    static int[] sampleArray() {
+        Random rd = new Random();
+        int[] sample = new int[100];
+        for (int i = 0; i < 100; i++) {
+            sample[i] = rd.nextInt();
+        }
+        return sample;
     }
-    return sample;
-  }
 
-  @Test
-  void testMakeHeap() {
-    int[] sample = sampleArray();
-    Main.makeHeap(sample);
-    for (int i = 1; i < sample.length; i++) {
-      Assertions.assertTrue(sample[i] <= sample[(i - 1) / 2], "Heap invariant is violated");
+    @Test
+    void testMakeHeap() {
+        int[] sample = sampleArray();
+        Main.makeHeap(sample);
+        for (int i = 1; i < sample.length; i++) {
+            Assertions.assertTrue(sample[i] <= sample[(i - 1) / 2], "Heap invariant is violated");
+        }
     }
-  }
 
-  @Test
-  void testHeapsort() {
-    int[] expected = sampleArray();
-    int[] actual = expected.clone();
-    Arrays.sort(expected);
-    Main.heapsort(actual);
-    Assertions.assertArrayEquals(expected, actual);
-  }
+    @Test
+    void testHeapsort() {
+        int[] expected = sampleArray();
+        int[] actual = expected.clone();
+        Arrays.sort(expected);
+        Main.heapsort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
