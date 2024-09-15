aa85c09cc15804334dc5fab737119972aa213012
Nikitat0 <nikitatzero@yandex.ru>
2024-09-15 13:15:39 +0700

    Setup gradle project

---
+++ Task_1_1_2/src/main/java/ru/nikitat0/blackjack/Main.java
@@ -0,0 +1,13 @@
+package ru.nikitat0.blackjack;
+
+/** Contains static method main. */
+public class Main {
+    /**
+     * Starts with program.
+     *
+     * @param args Program arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
